02b321694cdb6b596fe00eb256d55d6f2067cd0e
Martina Scholz <martina@simplysmart-it.de>
2024-09-06 04:04:24 +0200

    ✨ Add basic methods to First Entity Controller class for managing forms and records.
    Signed-off-by: Martina Scholz <martina@simplysmart-it.de>

--- component/com_{{cookiecutter.project_slug}}/src/administrator/components/com_{{cookiecutter.project_slug}}/src/Controller/{{cookiecutter.__entity.capitalize()}}Controller.php
+++ component/com_{{cookiecutter.project_slug}}/src/administrator/components/com_{{cookiecutter.project_slug}}/src/Controller/{{cookiecutter.__entity.capitalize()}}Controller.php
@@ -2,3 +2,103 @@
 {% include "docHeader.txt" ignore missing %}
 
 namespace {{cookiecutter.namespace}}\Component\{{cookiecutter.__project_camelcaps}}\Administrator\Controller;
+
+use Joomla\CMS\MVC\Controller\FormController;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Versioning\VersionableControllerTrait;
+use Joomla\Utilities\ArrayHelper;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * {{cookiecutter.__entity.capitalize()}} controller class.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class {{cookiecutter.__entity.capitalize()}}Controller extends FormController
+{
+    use VersionableControllerTrait;
+
+    /**
+     * The prefix to use with controller messages.
+     *
+     * @var    string
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $text_prefix = 'COM_{{cookiecutter.project_slug.upper()}}_{{cookiecutter.__entity.upper()}}';
+
+    /**
+     * Method override to check if you can add a new record.
+     *
+     * @param   array  $data  An array of input data.
+     *
+     * @return  boolean
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    protected function allowAdd($data = [])
+    {
+        $filter     = $this->input->getInt('filter_category_id');
+        $categoryId = ArrayHelper::getValue($data, 'catid', $filter, 'int');
+
+        if ($categoryId) {
+            // If the category has been passed in the URL check it.
+            return $this->app->getIdentity()->authorise('core.create', $this->option . '.category.' . $categoryId);
+        }
+
+        // In the absence of better information, revert to the component permissions.
+        return parent::allowAdd($data);
+    }
+
+    /**
+     * Method override to check if you can edit an existing record.
+     *
+     * @param   array   $data  An array of input data.
+     * @param   string  $key   The name of the key for the primary key.
+     *
+     * @return  boolean
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    protected function allowEdit($data = [], $key = 'id')
+    {
+        $recordId   = (int) isset($data[$key]) ? $data[$key] : 0;
+        $categoryId = 0;
+
+        if ($recordId) {
+            $categoryId = (int) $this->getModel()->getItem($recordId)->catid;
+        }
+
+        if ($categoryId) {
+            // The category has been set. Check the category permissions.
+            return $this->app->getIdentity()->authorise('core.edit', $this->option . '.category.' . $categoryId);
+        }
+
+        // Since there is no asset tracking, revert to the component permissions.
+        return parent::allowEdit($data, $key);
+    }
+
+    /**
+     * Method to run batch operations.
+     *
+     * @param   string  $model  The model
+     *
+     * @return  boolean  True on success.
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function batch($model = null)
+    {
+        $this->checkToken();
+
+        // Set the model
+        $model = $this->getModel('{{cookiecutter.__entity.capitalize()}}', '', []);
+
+        // Preset the redirect
+        $this->setRedirect(Route::_('index.php?option=com_{{cookiecutter.project_slug}}&view={{cookiecutter.__entities.lower()}}' . $this->getRedirectToListAppend(), false));
+
+        return parent::batch($model);
+    }
+}
